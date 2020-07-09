a3755eeabcebb33c2b57d58257b28301facc81cf
nairelyhz <njhs694@gmail.com>
2020-07-09 01:21:10 -0400

    all apis done

--- services/contacts/contacts.js
+++ services/contacts/contacts.js
@@ -1,9 +1,14 @@
 function contacts(params, admin){
+    
     const app = params;
-
+    const ContactsController = require('./contactsController');
+    const controller = new ContactsController(admin);
+    
     // Get all contact
-    app.get('/contacts', function (req, res) {
-        res.send('[GET]Saludos desde express');
+    app.get('/contacts',async (req, res) => {
+        const allContacts = await controller.getAllContacts(req.body);
+        console.log(allContacts);
+        res.send({status: 200, message: 'All contacts', data: allContacts});
     });
     
     // Get a contact
@@ -12,20 +17,32 @@ function contacts(params, admin){
         res.send('[GET]Saludos desde express a '+ req.params.contactId);
     });
     
-    //Create a contact
-    app.post('/contact', function (req, res) {
-        res.send('[POST]Saludos desde express');
+    //Add a contact
+    app.post('/contact', async (req, res)=> {
+        const contact = await controller.addContact(req.body);
+        res.send({status: 200, message: 'contact added', data: contact});
     });
     
     //Update a contact
     app.put('/contact', function (req, res) {
-        res.send('[POST]Saludos desde express');
+        const contact = controller.updateContact(req.body);
+        if(contact){
+            res.send({status: 200, message: 'Contact updated', data: req.body});
+        }else{
+            res.send({status: 500, message: 'Try later'});
+
+        };
     });
     
     //Delete a contact
     app.delete('/contact', function (req, res) {
-        res.send('[POST]Saludos desde express');
-    });
+        const contact = controller.deleteContact(req.body);
+        if(contact){
+            res.send({status: 200, message: 'Contact deleted', data: contact});
+        }else{
+            res.send({status: 500, message: 'Try later'});
+
+        };    });
   
 }
 
--- services/contacts/contactsController.js
+++ services/contacts/contactsController.js
@@ -3,27 +3,62 @@
 class contactsController{
 
     constructor(admin){
-        this.db = db.database();
+        this.admin = admin.database();
     }
     
-    createUser(data){
-        this.db.ref('contacts').push(data);
-        console.log("user created");
+    async getAllContacts(){
+        const snap = await getAllContactsFromFirebase();
+        this.exportLikeArray(snap);
+    }
 
+    async getAllContactsFromFirebase(){
+        const eventref = this.admin.ref('contacts');
+        const snapshot = await eventref.once('value');
+        return snapshot;
+    }
+
+    addContact(data){
+        this.admin.ref('contacts').push(data);
+        console.log("user created");
         return true;
     }
 
-    updateUser(){
-
+    async updateContact(data){
+        const contacts = await this.getAllContactsFromFirebase();
+        contacts.forEach((item)=>{
+            const itemValue = item.val();
+            if(data.email == itemValue.email){
+                this.admin.ref('contacts/'+item.key).set(data);
+                return true;
+            }
+        });
+        return false;
     }
 
-    passwordRecovery(){
-
+    async deleteContact(data){
+        const contacts = await this.getAllContactsFromFirebase();
+        contacts.forEach((item)=>{
+            const itemValue = item.val();
+            if(data.email == itemValue.email){
+                this.admin.ref('contacts/'+item.key).remove();
+                return true;
+            }
+        });
+        return false;
     }
 
-    login(){
-
+    exportLikeArray(snap){
+        const value = [];
+        snap.forEach(function(item) {
+            var itemVal = item.val();
+            value.push(itemVal);
+        });
+        return value;
     }
+
+    
+
+    
 }
 
 module.exports = contactsController;
